ff0083e986507addc4fe54108cefcce9b676b87f
claudiahunt43 <csh5487@psu.edu>
2024-12-09 15:13:01 -0500

    Update and rename spaceship.h to Spacecraft.h

---
+++ Spacecraft.h
@@ -0,0 +1,24 @@
+#pragma once
+#define GL_SILENCE_DEPRECATION
+#ifndef Spaceship_H
+#define Spaceship_H
+
+#include <cmath>
+#include <atomic>
+#include <OpenGL/gl.h>
+#include <GLUT/glut.h>
+
+class Spacecraft 
+{
+private:
+    std::atomic<float> positionX;
+    std::atomic<float> positionY;
+    static constexpr float squareSize = 50.0;
+
+public:
+    // Spacecraft constructor set to initial position
+    Spacecraft() : positionX(0.0), positionY(0.0) {}
+    void draw(float Xposition, float Yposition);
+};
+
+#endif // Spacecraft_H
--- spaceship.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#define GL_SILENCE_DEPRECATION
-#ifndef Spaceship_H
-#define Spaceship_H
-
-#include <cmath>
-#include <atomic>
-#include <OpenGL/gl.h>
-#include <GLUT/glut.h>
