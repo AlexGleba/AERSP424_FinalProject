76774ca69a16b723945dbc351851a5be0dc992c4
claudiahunt43 <csh5487@psu.edu>
2024-12-09 17:44:10 -0500

    Update Asteroid.h

--- Asteroid.h
+++ Asteroid.h
@@ -1,1 +1,32 @@
-# Asteroid
+ALEX CHECK 
+
+#ifndef ASTEROID_H
+#define ASTEROID_H
+
+#include <cmath>
+#include <atomic>
+
+#include <OpenGL/gl.h>
+#include <GLUT/glut.h>
+
+class Asteroid 
+{
+private:
+    std::atomic<float> positionX_asteroid;
+    std::atomic<float> positionY_asteroid;
+    static constexpr float squareSize = 50.0;
+    float Xa_position;
+    float Ya_position;
+
+public:
+    // Asteroid constructor to set initial position to 0
+    Asteroid() : Xa_position(0.0), Ya_position(0.0) {}
+
+    void draw(float Xposition, float Yposition);
+
+    float getXaPosition() const { return positionX_asteroid.load(); }
+    float getYaPosition() const { return positionY_asteroid.load(); }
+
+};
+
+#endif // ASTEROID_H
