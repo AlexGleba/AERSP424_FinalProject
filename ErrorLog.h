2a3b7008fa462dbfa9cb1cc7c2a1d6024800d465
Andreasgandhi <asg5725@psu.edu>
2024-12-11 19:49:14 -0500

    Update ErrorLog.h

--- ErrorLog.h
+++ ErrorLog.h
@@ -1,52 +1,58 @@
-#ifndef LOGGER_H
-#define LOGGER_H
+// #ifndef LOGGER_H
+// #define LOGGER_H
+
+#ifndef ERRORLOG_H
+#define ERRORLOG_H
 
 #include <iostream>
 #include <fstream>
 #include <string>
 #include <ctime>
 
-class ErrorLogger {
+class ErrorLog
+{
 public:
     // Constructor: Initializes the log file
-    ErrorLogger(const std::string& logFileName = "error_log.txt") 
-{
+    ErrorLog(const std::string &logFileName = "error_log.txt")
+    {
         logFile.open(logFileName, std::ios::out | std::ios::app); // Opens the log file in append mode
-        if (!logFile.is_open()) 
+        if (!logFile.is_open())
         {
             std::cerr << "Failed to open log file: " << logFileName << std::endl;
         }
     }
 
     // Destructor: Closes the log file
-    ~ErrorLogger() 
-{
-        if (logFile.is_open()) 
+    ~ErrorLog()
+    {
+        if (logFile.is_open())
         {
             logFile.close();
         }
     }
 
     // Writes an error message to the log file
-    void WriteError(const std::string& errorMessage) 
-{
-        if (logFile.is_open()) 
+    void WriteError(const std::string &errorMessage)
+    {
+        if (logFile.is_open())
         {
             logFile << getCurrentTime() << " - ERROR: " << errorMessage << std::endl;
-        } else 
+        }
+        else
         {
             std::cerr << "Error logging failed. Could not write to log file." << std::endl;
         }
     }
 
-    // Write a general message 
+    // Write a general message
     // Used for warnings, info, etc.
-    void WriteMessage(const std::string& message) 
-{
-        if (logFile.is_open()) 
+    void WriteMessage(const std::string &message)
+    {
+        if (logFile.is_open())
         {
             logFile << getCurrentTime() << " - INFO: " << message << std::endl;
-        } else 
+        }
+        else
         {
             std::cerr << "Error logging failed. Could not write to log file." << std::endl;
         }
@@ -55,12 +61,12 @@ public:
 private:
     std::ofstream logFile;
 
-    // Get the current date and time as a string 
+    // Get the current date and time as a string
     // format: YYYY-MM-DD HH:MM:SS
-    std::string getCurrentTime() 
-{
+    std::string getCurrentTime()
+    {
         time_t Time;
-        struct tm* time_info;
+        struct tm *time_info;
         char buffer[80];
 
         time(&Time);
@@ -71,4 +77,4 @@ private:
     }
 };
 
-#endif // LOGGER_H
+#endif // ERRORLOG_H
