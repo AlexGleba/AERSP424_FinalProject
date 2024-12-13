0e3a714d3ae71d49408f9abd03f0a49ab75da549
Andreasgandhi <asg5725@psu.edu>
2024-12-12 23:02:15 -0500

    Update Asteroid.h

--- Asteroid.h
+++ Asteroid.h
@@ -22,7 +22,7 @@ class Asteroid
 private:
     // std::atomic<float> positionX_asteroid;
     // std::atomic<float> positionY_asteroid;
-    static constexpr float square_size = 50.0;
+    static constexpr float square_size = 10.0;
     float Xa_position;
     float Ya_position;
 
@@ -43,7 +43,7 @@ public:
 
     void draw()
     {
-        glColor3f(0.0, 1.0, 1.0);
+        glColor3f(0.0, 0.0, 1.0);
         glBegin(GL_POLYGON);
         for (int i = 0; i < 360; i += 30)
         {
@@ -64,77 +64,6 @@ public:
         }
     }
 
-    /* float x, y;
-     int sizevalue;
-     AsteroidSize size;
-
-     // Asteroid constructor to set initial position to 0
-     Asteroid() : Xa_position(0.0), Ya_position(0.0) {}
-
-
-     explicit Asteroid(AsteroidSize size) : size(size)
-     {
-         switch(size)
-         {
-             case AsteroidSize::BIG: sizevalue = 4;
-             break;
-             case AsteroidSize::SMALL: sizevalue = 6;
-             break;
-         }
-     }
-
-
-     //void draw(float Xposition, float Yposition);
-     //void updateAsteroids(int value);
-     //void renderAsteroids();
-
- static std::vector<std::unique_ptr<Asteroid>> initAsteroids(int numberasteroids)
- {
-     // vector of Asteroids
-     std::vector<std::unique_ptr<Asteroid>> asteroids;
-     // int number_asteroids = 0;
-     int numberasteroids;
-     if (numberasteroids<=20)
-     {
-         numberasteroids += 1;
-     }
-
-     for(int i =0; i < numberasteroids; i++)
-     {
-           auto size = static_cast<AsteroidSize>(std::rand() % 2);
-           auto asteroid = std::make_unique<Asteroid>(size);
-           asteroids.push_back(std::move(asteroid));
-     }
-     return asteroids;
- }
-
-     void initShapeAsteroid()
-     {
-         float speed;
-         float base;
-
-         switch(size)
-         {
-             case AsteroidSize::BIG: speed = 20.0f;
-             base = 20.0f;
-             break;
-             case AsteroidSize::SMALL: speed = 30.0f;
-             base = 10.0f;
-             break;
-         }
-
-         glColor3f(1.0,1.0,1.0);
-         glBegin(GL_POLYGON);
-         for (int i = 0; i < 360; i += 30)
-         {
-             float angle = i * M_PI / 180; // convert to radians
-             glVertex2f(cos(angle) * base, sin(angle) * base); // Draw a circle for the Asteroid
-         }
-         glEnd();
-     //shape.setPosition(std::rand() % x, y)
-
-    // float getXaPosition() const { return positionX_asteroid.load(); }
-    // float getYaPosition() const { return positionY_asteroid.load(); } */
 };
 
 #endif // ASTEROID_H
